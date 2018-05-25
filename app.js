efb21cee48c5f1cff6f7a5fc19e2ec2f7c1c2643
Todd Brannon <toddqbrannon@gmail.com>
2018-05-25 18:55:30 -0500

    setup

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/assets'));
+
+app.get('/', (req, res) => {
+    res.render('landing');
+});
+
+app.get('/main', (req, res) => {
+    res.render('main');
+});
+
+app.get('/portfolio', (req, res) => {
+    res.render('portfolio');
+});
+
+app.listen(port);
+
+//const port = (process.env.PORT, process.env.IP) || 5000;
+
+//app.listen(port, () => {
+//console.log(`TB server started on port ${port}`)
+//});
